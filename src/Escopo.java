1f936d63feb2986656618e4c92bd8febbaceb68a
Ezequiel <ezequielsilvalima01@gmail.com>
2024-10-01 21:23:23 -0300

    commit

--- src/Escopo.java
+++ src/Escopo.java
@@ -2,7 +2,7 @@ import java.security.KeyStore;
 
 public class Escopo {
     public static void main(String[] args) {
-        //Escopo: evita conflitos de nomes, liberar memória quando variáveis locais não são mais necessárias e impede o acesso não intencional a variáveis
+        //Escopo: evita conflitos de nomes, libera memória quando variáveis locais não são mais necessárias e impede o acesso não intencional a variáveis
         int x;
         if (true) {
             x = 2; //Variável local
---
+++ src/VarImutavel.java
@@ -0,0 +1,7 @@
+public class VarImutavel {
+    public static void main(String[] args) {
+        //Variável Imutável
+        final double PI = 3.14159; // final: Armazena um valor fixo e não permite que seja alterado
+                                  // double: Utilizado para armazenar valores númericos decimais
+    }
+}

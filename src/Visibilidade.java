c4eeaac5485f3ce9832abd90cd8d505c875f368c
Ezequiel <ezequielsilvalima01@gmail.com>
2024-10-19 16:34:45 -0300

    commit

--- src/Visibilidade.java
+++
@@ -1,9 +0,0 @@
-public class Visibilidade {
-    public static void main(String[] args) {
-       //public: Visibilidade do >{escopo}< do projeto
-        int x;
-    }
-    public static void sum (int a, int b){
-     System.out.println(x); //<<--- ocorre um erro ao executar  a variável, ja que ela é de outro escopo
-    }
-}
--- src/com/ezequielapp/app/User.java
+++
@@ -1,9 +0,0 @@
-package com.ezequielapp.app;
-import com.ezequiel.App;
-
-public class User {
-
-    public static void println(){
-        System.out.println(App.x);
-    }
-}

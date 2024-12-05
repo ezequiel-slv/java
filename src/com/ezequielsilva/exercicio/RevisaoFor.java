739a08818908a8b002d6a9950a598d3c69ec8a3c
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-05 09:10:59 -0300

    commit

---
+++ src/com/ezequielsilva/exercicio/RevisaoFor.java
@@ -0,0 +1,13 @@
+package com.ezequielsilva.exercicio;
+
+public class RevisaoFor {
+    public static void main(String[] args) {
+        //imprima os numeros pares de 1 a 1000000
+
+        for (int i = 0; i <= 1000000; i++) {
+           if (i % 2 == 0);{
+            System.out.println(i);
+         }
+        }
+    }
+}

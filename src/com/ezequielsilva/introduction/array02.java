507461ea77e09c29cafbf2eddbe7a3a7174a15a1
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-13 15:01:56 -0300

    commit

---
+++ src/com/ezequielsilva/introduction/array02.java
@@ -0,0 +1,14 @@
+package com.ezequielsilva.introduction;
+
+public class array02 {
+    public static void main(String[] args) {
+
+        String[] nomes = new String[3];
+
+        nomes[0] = "Ezequiel";
+        nomes[1] = "Silva";
+        nomes[2] = "Lima";
+
+        System.out.println(nomes[0]+' '+ nomes[1]+' '+ nomes[2]);
+    }
+}

ba70f2c2a3f483525922cd8e1f1a48981a143a3d
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-13 14:48:03 -0300

    commit

---
+++ src/com/ezequielsilva/introduction/Array.java
@@ -0,0 +1,14 @@
+package com.ezequielsilva.introduction;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 30;
+        idades[2] = 25;
+        
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}

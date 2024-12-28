dc235dd9b1e6ef8d2d4c749fa88f670a081b045a
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-28 20:19:30 -0300

    commit

--- src/com/ezequielsilva/introduction/Array05Forech.java
+++ src/com/ezequielsilva/introduction/Array05Forech.java
@@ -1,10 +1,8 @@
 package com.ezequielsilva.introduction;
 
-public class Array04 {
+public class Array05Forech {
     public static void main(String[] args) {
-        // 1, 2, 3, 4, 5, 6 meses
-        // 1, 2, 3, 4, 5, dias
-
+        
         int[][] dias = new int[3][3];
         dias[0][0] = 31;
         dias[0][1] = 28;
@@ -13,9 +11,10 @@ public class Array04 {
         dias[1][0] = 31;
         dias[1][1] = 28;
         dias[1][2] = 31;
-        for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[i].length; j++) {
-                System.out.println(dias[i][j]);
+
+        for (int[] arrBase : dias) {
+            for (int num : arrBase) {
+                System.out.println(num);
             }
         }
     }
